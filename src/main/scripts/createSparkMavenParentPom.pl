69cf3ce3b4dcc72810f28238fb2aca2fbc36ef05
Christophe Préaud <christophe.preaud@kelkoogroup.com>
2020-05-11 11:10:00 +0200

    Generate only dependencyManagement and properties sections

--- src/main/scripts/createSparkMavenParentPom.pl
+++ src/main/scripts/createSparkMavenParentPom.pl
@@ -1,15 +1,11 @@
 #!/usr/bin/perl
 
+use Getopt::Long;
 use strict;
 use warnings;
 
 # needed to get the list of spark / hadoop jars dependencies
-my $REMOTE_CMD = "ls -1 /opt/kookel/{kelkooHadoop*,kelkooSparkLib}/current/javalib/*.jar";
-my $SPARK_HOST="dc1-kdp-prod-spark-01.prod.dc1.kelkoo.net";
-
-# pom parents version
-my $KELKOO_SPARK_MAVEN_PARENT_VERSION = "1.2.1-SNAPSHOT";
-my $KELKOO_MAVEN_PARENT_VERSION = "2.9.1";
+my $REMOTE_CMD = "ls -1 {/opt/hadoop/share/hadoop/*/lib,/opt/spark/jars}/*.jar";
 
 # pom properties
 my $HADOOP_VERSION = "2.8.3";
@@ -19,33 +15,7 @@ my $SCALA_VERSION = "2.11.12";
 my $SHORT_SCALA_BINARY_VERSION = "11";
 my $SPARK_VERSION = "2.4.0";
 
-my $pom_header = qq(<?xml version="1.0" encoding="UTF-8"?>
-<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">
-
-   <!-- PROJECT DESCRIPTION SECTION -->
-   <modelVersion>4.0.0</modelVersion>
-   <groupId>com.kelkoogroup.dataplatform</groupId>
-   <artifactId>kelkooSparkMavenParent</artifactId>
-   <version>$KELKOO_SPARK_MAVEN_PARENT_VERSION</version>
-   <packaging>pom</packaging>
-   <name>kelkooSparkMavenParent</name>
-   <description>Common component for dataplatform applications</description>
-
-   <!-- PARENT SECTION -->
-   <parent>
-      <groupId>com.kelkoo</groupId>
-      <artifactId>kelkooMavenParent</artifactId>
-      <version>$KELKOO_MAVEN_PARENT_VERSION</version>
-   </parent>
-
-   <!-- Source control management SECTION -->
-   <scm>
-      <connection>scm:git|git\@gitlab.corp.kelkoo.net:data-platform/kelkoosparkmavenparent.git</connection>
-      <url>http://gitlab.corp.kelkoo.net/data-platform/kelkoosparkmavenparent</url>
-      <tag>HEAD</tag>
-   </scm>
-
-   <!-- DEPENDENCIES SECTION -->
+my $pom_header = qq(   <!-- DEPENDENCIES SECTION -->
    <dependencyManagement>
       <dependencies>
 
@@ -69,13 +39,10 @@ my $pom_footer = qq(         <!-- Test dependencies -->
       <scala.version>$SCALA_VERSION</scala.version>
       <short.scala.binary.version>$SHORT_SCALA_BINARY_VERSION</short.scala.binary.version>
       <spark.version>$SPARK_VERSION</spark.version>
-   </properties>
-
-</project>);
+   </properties>);
 
 sub get_jar_name_and_version($) {
   my ($jar) = @_;
-  #my ($jar_name, $jar_version) = ($jar =~ /^([\w\-\.]+)-((?:\d+\.)*\d+(?:-(?:\d|3f79e055|GA|M15|M20|b34|hadoop2|incubating|nohive|shaded-protobuf|tests)|\.(?:Final|RELEASE|Release|spark2))?)\.jar$/);
   my ($jar_name, $jar_version) = ($jar =~ /^([\w\-\.]+)-((?:\d+\.)*\d+(?:[-\.]\S+)?)\.jar$/);
   return ($jar_name, $jar_version);
 }
@@ -103,9 +70,16 @@ sub get_group_and_artifact($$) {
   return ($group_id, $artifact_id);
 }
 
+sub printUsage() {
+  print STDERR "Usage:\n\t$0 -hostname HOSTNAME\n";
+  exit 1;
+}
+
+my $hostname = undef;
+printUsage() unless (GetOptions("hostname=s" => \$hostname) and defined $hostname);
+
 my $jar_list = {};
-#my $cmd = "ssh $SPARK_HOST \"$REMOTE_CMD\"";
-my $cmd = "/home/preaudc/vendor-src/spark-maven-parent/src/main/scripts/readPOM.pl | grep -v spark-testing-base";
+my $cmd = "ssh $hostname \"$REMOTE_CMD\"";
 open(CMD, "$cmd |") or die "$!";
 while (my $cmd_line = <CMD>) {
   chomp $cmd_line;
@@ -122,16 +96,6 @@ while (my $cmd_line = <CMD>) {
   }
 }
 close CMD;
-
-#my $artifact_id = "jackson-core-asl";
-#my $version = $jar_list->{$artifact_id};
-#print "$artifact_id - $version\n";
-#open(CMD, "ssh $SPARK_HOST \"unzip -q -c /opt/spark/jars/$artifact_id-$version.jar META-INF/MANIFEST.MF | grep ^Bundle-SymbolicName\" |") or die "$!";
-#while (my $cmd_line = <CMD>) {
-#  chomp $cmd_line;
-#  print "$cmd_line\n";
-#}
-#close CMD;
 
 my @dependencies = ();
 foreach my $k (keys %$jar_list) {
